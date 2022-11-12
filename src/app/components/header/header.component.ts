71771f1765a6865fe49e3716bc2007a8516d92da
Platon Lumpov <ut.asanh@gmail.com>
2022-11-12 16:34:35 +0600

    Header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  async goTo(link: string) {
+    await this.router.navigate([`main/${link}`]);
   }
-
 }
